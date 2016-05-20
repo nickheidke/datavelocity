bf04cabfccce22f3e3a4d8e5baa2c7617ffb2667
nickheidke <nick.heidke@gmail.com>
2016-05-20 08:50:24 -0500

    Implement IDisposable on Database class, fix for output directory

--- dmaDataAccess/Database.cs
+++ dmaDataAccess/Database.cs
@@ -1,22 +1,46 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public abstract class Database
+    public abstract class Database : IDisposable
     {
         protected string _connectionString;
+        bool _disposed = false;
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
         private SqlConnection _conn;
 
         public Database(string connectionString)
         {
             _connectionString = connectionString;
             _conn = new SqlConnection(connectionString);
+        }
+
+        public void Dispose() 
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                _connectionString = null;
+            }
+
+            _conn = null;
+            _disposed = true;
         }
 
         abstract public string getDatabase();
