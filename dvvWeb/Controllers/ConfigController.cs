4e29d5ef69189f4fb78d605d74a1f942f3a37a1c
nickheidke <nick.heidke@gmail.com>
2016-06-04 12:56:25 -0500

    Adding web project - initial

---
+++ dvvWeb/Controllers/ConfigController.cs
@@ -0,0 +1,20 @@
+﻿using dvvWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dvvWeb.Controllers
+{
+    public class ConfigController : Controller
+    {
+        //
+        // GET: /Config/
+        public ActionResult Index()
+        {
+            ConfigModel model = new ConfigModel();
+            return View(model);
+        }
+	}
+}
---
+++ dvvWeb/Models/ConfigModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using dvvModels;
+
+namespace dvvWeb.Models
+{
+    public class ConfigModel
+    {
+        dvvPrefsModel _preferences;
+
+        public dvvPrefsModel Preferences
+        {
+            get { return _preferences; }
+            set { _preferences = value; }
+        }
+        string _servername, _dbName;
+
+        public string DbName
+        {
+            get { return _dbName; }
+            set { _dbName = value; }
+        }
+
+        public string Servername
+        {
+            get { return _servername; }
+            set { _servername = value; }
+        }
+
+
+    }
+}
