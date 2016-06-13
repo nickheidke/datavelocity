0b3302fc4823119cf2270e4692d2fe6e275280c7
nickheidke <nick.heidke@gmail.com>
2016-06-13 10:10:31 -0500

    Moving long running operation to a task

--- dvvWeb/Hubs/ChartHub.cs
+++ dvvWeb/Hubs/ChartHub.cs
@@ -8,14 +8,24 @@ using dvvModels;
 using dvvHelpers;
 using System.Web.Mvc;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Web.Hosting;
+using System.Security.Principal;
 
 namespace dvvWeb.Hubs
 {
     public class ChartHub : Hub
     {
+        CancellationTokenSource tokenSource;
+        CancellationToken ct;
         public void Start(string serverName, string dbName, string numberOfPoints, string pollingFrequency)
         {
             ConfigModel config = new ConfigModel();
+
+            tokenSource = new CancellationTokenSource();
+            ct = tokenSource.Token;
 
             config.Servername = HttpUtility.UrlDecode(serverName);
             config.DbName = HttpUtility.UrlDecode(dbName);
@@ -28,13 +38,50 @@ namespace dvvWeb.Hubs
 
             graphingModel = graphingHelper.Tick(config.Preferences);
 
+            var identity = WindowsIdentity.GetCurrent();
 
-            while (true)
+            Task.Run(() => workItemAsync(ct, graphingModel, graphingHelper, config, identity));
+
+            /*while (true)
             {
                 Clients.Caller.addPointToChart(JsonConvert.SerializeObject(graphingModel));
                 System.Threading.Thread.Sleep(config.Preferences.PollingFrequency * 1000);
                 graphingModel = graphingHelper.Tick(config.Preferences);
             }
+             * */
+        }
+
+        public void Stop()
+        {
+            tokenSource.Cancel();
+        }
+
+
+        private async Task<CancellationToken> workItemAsync(CancellationToken ct, dvvGraphingModel graphingModel, dvvGraphingHelper graphingHelper, ConfigModel configModel, WindowsIdentity identity)
+        {
+            await addLogAsync(ct, graphingModel, graphingHelper, configModel, identity);
+            return ct;
+        }
+
+        private async Task<CancellationToken> addLogAsync(CancellationToken ct, dvvGraphingModel graphingModel, dvvGraphingHelper graphingHelper, ConfigModel configModel, WindowsIdentity identity)
+        {
+
+            try
+            {
+                while(!ct.IsCancellationRequested)
+                {
+                    identity.Impersonate();
+                    Clients.Caller.addPointToChart(JsonConvert.SerializeObject(graphingModel));
+                    System.Threading.Thread.Sleep(configModel.Preferences.PollingFrequency * 1000);
+                    graphingModel = graphingHelper.Tick(configModel.Preferences);
+
+                }
+            }
+            catch (TaskCanceledException tce)
+            {
+                Trace.TraceError("Caught TaskCanceledException - signaled cancellation " + tce.Message);
+            }
+            return ct;
         }
     }
 }
